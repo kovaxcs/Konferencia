848fefe239e2f97acef0ff5114b5c97661e47efc
kovaxcs <kovax2004@yahoo.com>
2013-07-19 10:18:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,27 +3,17 @@
 <head>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js" type="text/javascript" charset="utf-8"></script>
 <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-<style type="text/css">
-h3 {font-size:30px; font-weight:bold; text-transform:uppercase; margin-top:0;}
-p {font-size:15px; line-height: 200%;}
-#menus > a { text-decoration:none; color:#FFFFFF; padding-bottom:0.3%; margin-right:5%;}
-#menus > a:hover { border-bottom:#FFFFFF 1px solid;}
-#spacer {min-height:120px}
-@media screen and (max-width: 1100px) {
-   div#menus > a {font-size:14px; margin-right:4%; }
-   p {font-size:14px; line-height: 170%;}
-}
-</style>
+<link rel="stylesheet" href="style.css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Konferencia</title>
 </head>
-<body style="font-size:100%; padding:0px; margin:0px; background-color:#0046a5; font-family: 'Noto Sans', sans-serif; color:#FFFFFF">
-<div id="header" style="width:100%; overflow:auto;">
-  <div id="logo" style="width:19.2533%; float:left;  margin-left:6.0%; margin-top:2.0%">
-		<a href="index.php"><img src="logo.png" alt="logo" style="width:100%"/></a>
+<body>
+<div id="header">
+  <div id="logo">
+		<a href="index.php"><img src="logo.png" alt="logo"/></a>
     </div>
-    <div id="menus" style="width:60%; text-align:center; float:left; margin:9% 7% 0% 7%; font-size:15px; font-weight:bold; text-transform:uppercase;">
-    	<a style="border-bottom:#FFFFFF 1px solid;" href="#"> KÖSZÖNTŐ </a>
+    <div id="menus">
+    	<a href="#" class="active"> KÖSZÖNTŐ </a>
         <a href="#"> HATÁRIDŐK </a>
         <a href="#"> PROGRAM </a>
         <a href="#"> CÉLCSOPORT </a>
@@ -32,10 +22,10 @@ p {font-size:15px; line-height: 200%;}
     </div>
 </div>
 <div id="spacer"></div>
-<div id="left" style="float:left; width:50%; padding:0; margin:0;">
+<div id="left">
 	<img align="right" src="koszonto.png" alt="koszoto" style="width:75%; margin-top:45px;"/>
 </div>
-<div id="right" style="float:left; width:35%; margin:0; padding-left:4%; padding-right:11%">
+<div id="right">
 	<h3> KÖSZÖNTŐ </h3>
     <p>A konferenciára várjuk tehetséges főiskolai, egyetemi és Ph.D hallgatók jelentkezését a környezet- és természetvédelem, valamint a vidékfejlesztés bármely területét érintő előadásokkal és/vagy poszterek bemutatásával.</p>
 </div>
