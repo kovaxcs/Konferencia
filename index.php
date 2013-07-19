03f0017f337563291bb80b886a1f7a75a4b021a6
kovaxcs <kovax2004@yahoo.com>
2013-07-19 10:43:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 <head>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js" type="text/javascript" charset="utf-8"></script>
 <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="style.css">
+<link type="text/javascript" rel="stylesheet" href="style.css">
+<script type="text/javascript" src="main.js"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Konferencia</title>
 </head>
 <body>
 <div id="header">
   <div id="logo">
-		<a href="index.php"><img src="logo.png" alt="logo"/></a>
+	<a href="index.php"><img src="logo.png" alt="logo"/></a>
     </div>
     <div id="menus">
     	<a href="#" class="active"> KÖSZÖNTŐ </a>
@@ -22,12 +23,19 @@
     </div>
 </div>
 <div id="spacer"></div>
-<div id="left">
-	<img align="right" src="koszonto.png" alt="koszoto" style="width:75%; margin-top:45px;"/>
+<div id="container">
+	<div id="left">
+		<img align="right" src="koszonto.png" alt="koszoto" style="width:75%; margin-top:45px;"/>
+	</div>
+	<div id="right">
+		<h3 id="koszonto"> KÖSZÖNTŐ </h3>
+		<p>A konferenciára várjuk tehetséges főiskolai, egyetemi és Ph.D hallgatók jelentkezését a környezet- és természetvédelem, valamint a vidékfejlesztés bármely területét érintő előadásokkal és/vagy poszterek bemutatásával.</p>
+	</div>
 </div>
-<div id="right">
-	<h3> KÖSZÖNTŐ </h3>
-    <p>A konferenciára várjuk tehetséges főiskolai, egyetemi és Ph.D hallgatók jelentkezését a környezet- és természetvédelem, valamint a vidékfejlesztés bármely területét érintő előadásokkal és/vagy poszterek bemutatásával.</p>
+<div id="content">
+	<p>A Szolnoki Főiskola 2013. szeptember 27-én a hagyományokat folytatva, 19. alkalommal ismét megrendezi a NEMZETKÖZI KÖRNYEZETVÉDELMI ÉS VIDÉKFEJLESZTÉSI DIÁKKONFERENCIÁT.</p>
+	<p><strong>A konferenciára várjuk tehetséges főiskolai, egyetemi és Ph.D hallgatók jelentkezését a környezet- és természetvédelem, valamint a vidékfejlesztés bármely területét érintő előadásokkal és/vagy poszterek bemutatásával.</strong></p>
+	<p>A konferencia célja: a felsőoktatási intézmények főiskolai, egyetemi és Ph.D képzésben résztvevő hallgatói már a képzés ideje alatt lehetőséget kapjanak a szakmai bemutatkozásra, széles körű gondolatcserére, a hazai és nemzetközi tudományos kapcsolatok kiépítésére.</p>
 </div>
 </body>
 </html>
